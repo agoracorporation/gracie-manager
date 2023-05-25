3bc0b917a768728415ea76be462e2175aa97d677
RafaelPereira901 <rafaelpereira901@gmail.com>
2023-05-24 23:36:43 -0300

    Configured spring-doc swagger implementaion!

--- src/main/java/com/agoracorp/gracie/controller/StudentController.java
+++ src/main/java/com/agoracorp/gracie/controller/StudentController.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.agoracorp.gracie.model.Student;
 import com.agoracorp.gracie.service.StudentService;
 
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping(value="/gracie-manager/api/")
 public class StudentController {
 	
