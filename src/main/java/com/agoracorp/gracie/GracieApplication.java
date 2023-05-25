697a30eb60f13de220b1d3358898f589a6ebc8b5
RafaelPereira901 <rafaelpereira901@gmail.com>
2023-05-24 21:17:12 -0300

    Initial commit

---
+++ src/main/java/com/agoracorp/gracie/GracieApplication.java
@@ -0,0 +1,13 @@
+package com.agoracorp.gracie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GracieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GracieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agoracorp/gracie/GracieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agoracorp.gracie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GracieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
